0d570e00b7788846acb6578db21cec258ab06753
newboys30 <newboys_zuiai@outlook.com>
2018-09-26 17:10:12 +0800

    kafka frist commit

---
+++ KafkaService/src/main/java/com/bioFish/KafkaService/execute/KafkaDaoSender.java
@@ -0,0 +1,15 @@
+package com.bioFish.KafkaService.execute;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * kafka发送
+ * @ClassName: KafkaSender
+ * @Description: TODO
+ * @author: newbo
+ * @date: 2018年9月26日 下午5:06:25
+ */
+@Component
+public class KafkaDaoSender {
+	
+}
