016c51249b2ef8877c741be810a8e55925096e3d
newboys30 <newboys_zuiai@outlook.com>
2018-09-30 11:53:35 +0800

    mybatis commit

--- ReadAndWrite/src/main/java/com/wtl/DAOService/Execute/UserExecute.java
+++ ReadAndWrite/src/main/java/com/wtl/DAOService/Execute/UserExecute.java
@@ -1,20 +1,24 @@
 package com.wtl.DAOService.Execute;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wtl.DAOService.dao.UserMapper;
+import com.wtl.DAOService.Entity.User;
+import com.wtl.DAOService.util.DaoSupport;
+import com.wtl.Utils.JsonUtil;
 
 @RestController
 public class UserExecute {
 	
-	@Autowired
-	private UserMapper userMapper;
+	@Resource
+	private DaoSupport dao;
 	
 	@GetMapping("/findAllUsers/{user_name}")
-	public String getUser(@PathVariable String user_name) {
-		return userMapper.selectByName(user_name).toString();
+	public String getUser(@PathVariable String user_name) throws Exception{
+		User user = (User) dao.findForObject("UserMapper.selectByName", user_name);
+		return JsonUtil.createGsonString(user);
 	}
 }
