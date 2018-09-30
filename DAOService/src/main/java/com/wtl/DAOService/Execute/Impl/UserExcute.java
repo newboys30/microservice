eccf34aed3c04cc61c8d85c9be9019d88f630713
newboys30 <newboys_zuiai@outlook.com>
2018-09-30 09:13:33 +0800

    update

--- DAOService/src/main/java/com/wtl/DAOService/Execute/Impl/UserExcute.java
+++ DAOService/src/main/java/com/wtl/DAOService/Execute/Impl/UserExcute.java
@@ -4,12 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.wtl.DAOService.Entity.T_user;
 import com.wtl.DAOService.Repository.UserRepository;
