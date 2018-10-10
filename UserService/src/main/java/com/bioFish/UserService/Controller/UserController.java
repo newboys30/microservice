f794302ae90582a4e2117a3a2dbe2f2ef2bc0d43
newboys30 <newboys_zuiai@outlook.com>
2018-10-10 18:16:55 +0800

    数据库、kafka配置中心有问题

--- UserService/src/main/java/com/bioFish/UserService/Controller/UserController.java
+++ UserService/src/main/java/com/bioFish/UserService/Controller/UserController.java
@@ -4,15 +4,13 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bioFish.UserService.Service.UserService;
 import com.bioFish.Params.WebParam;
+import com.bioFish.UserService.Service.UserService;
 import com.bioFish.Utils.JsonUtil;
 
 @RestController
