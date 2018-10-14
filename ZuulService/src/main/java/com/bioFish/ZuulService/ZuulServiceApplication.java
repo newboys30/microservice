1f169b1739ae3d9d4a7bb71332f31ecfb11cd3e0
newboys30 <newboys_zuiai@outlook.com>
2018-10-14 23:27:17 +0800

    zuul

--- ZuulService/src/main/java/com/bioFish/ZuulService/ZuulServiceApplication.java
+++ ZuulService/src/main/java/com/bioFish/ZuulService/ZuulServiceApplication.java
@@ -2,8 +2,10 @@ package com.bioFish.ZuulService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
