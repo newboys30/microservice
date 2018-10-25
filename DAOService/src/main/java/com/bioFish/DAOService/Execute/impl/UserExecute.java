0ebb4c7088b855fc5bed51cb31914dc70328a049
newboys30 <newboys_zuiai@outlook.com>
2018-10-25 14:36:18 +0800

    修改SQL请求方式

--- DAOService/src/main/java/com/bioFish/DAOService/Execute/impl/UserExecute.java
+++ DAOService/src/main/java/com/bioFish/DAOService/Execute/impl/UserExecute.java
@@ -3,16 +3,17 @@ package com.bioFish.DAOService.Execute.impl;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bioFish.DAOService.DaoSupport.DaoSupport;
 import com.bioFish.DAOService.DataSource.ReadDataSource;
 import com.bioFish.DAOService.DataSource.WriteDataSource;
 import com.bioFish.DAOService.Entity.User;
-import com.bioFish.DAOService.dao.usermapper.UserMapper;
 import com.bioFish.DAOService.util.SpringContextUtil;
 import com.bioFish.Utils.JsonUtil;
 import com.github.pagehelper.Page;
@@ -30,13 +31,13 @@ import com.github.pagehelper.PageInfo;
 @Service("userExecute")
 public class UserExecute {
 	
-	@Autowired
-	private UserMapper userMapper;
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
 	
 	@ReadDataSource
 	public User getUserByName(String jsonStr) throws Exception{
 		String user_name = JsonUtil.changeGsonToBean(jsonStr, String.class);
-		return userMapper.selectByName(user_name);
+		return (User) dao.findForObject("UserMapper.selectByName", user_name);
 	}
 	
 	@Transactional(propagation=Propagation.REQUIRED,isolation=Isolation.DEFAULT,readOnly=false)
@@ -44,7 +45,7 @@ public class UserExecute {
 	public Map<String,String> insertUser(String jsonStr) throws Exception{
 		Map<String,String> retMap = new HashMap<String,String>();
 		User user = JsonUtil.changeGsonToBean(jsonStr, User.class);
-		int i = userMapper.saveUser(user);
+		int i = (int) dao.save("UserMapper.saveUser", user);
 		if(0 != i) {
 			retMap.put("retMsg", "保存成功");
 			retMap.put("retFlag", "1");
@@ -72,7 +73,7 @@ public class UserExecute {
 		int pageSize = pageMap.get("pageSize") == null?20:Integer.parseInt(pageMap.get("pageSize").toString());
 		String username = pageMap.get("username").toString();
 		Page<User> page = PageHelper.startPage(pageNum, pageSize);
-		userMapper.selectAll(username);
+		dao.findForList("UserMapper.selectAll", username);
 		return page.toPageInfo();
 	}
 	
